138bc05d086e4decbad3415147222bf95a6d0600
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-07-18 14:18:27 +0200

    element d'affichage galerie de travaux

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -4,4 +4,7 @@ fetch("http://localhost:5678/api");
 
 fetch(`http://localhost:8081/pieces/${id}/travaux`);
 
-
+//ajouter galerie de travaux récuperés avec creat Element
+const article = travaux[0];
+const imageElement = document.createElement("img");
+travauxElement.src = article.travaux;
