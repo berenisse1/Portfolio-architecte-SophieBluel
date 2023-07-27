30bb5a5f4d496431dee35a114eda0393763c9a51
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-07-27 17:45:35 +0200

    apl api affichage gallery

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,3 +1,47 @@
+let urlWorks = `http://localhost:5678/api/works`; // on recupere l'api work
+let section = document.querySelector('.gallery');// on va chercher la class gallery pour aficher les travaux
+const btnFiltre = document.querySelector(btnFiltre[data-id])
 
+
+const apiCall = async() => {
+    await fetch(urlWorks) // pour faire attendre pendant le traitement
+    .then((response) => response.json ()) // réponse requête transformer au format Json
+    .then((data) => (apiData = data)) // cherche les donnés de l'api
+
+    function showWork(apiData){
+
+        apiData.forEach(element => { //boucle crée 1 par élement
+
+            newFigure = document.createElement('figure');
+            newFigure.id = element.id;
+
+            newImg = document.createElement('img');
+            newImg.src = element.imageUrl;
+            newImg.alt = element.title;
+
+            newFigcaption = document.createElement ('figcaption');
+            newFigcaption.innerText = element.title;
+
+            section.appendChild(newFigure);
+            newFigure.appendChild(newImg);
+            newFigure.appendChild(newFigcaption);
+
+
+        });
+
+       
+    
+           
+       
+    }
+
+    showWork (apiData);
+
+
+    
+
+}
+
+apiCall();
 
 
