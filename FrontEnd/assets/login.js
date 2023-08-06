90413c079be66cb6971dca029a47e46676c6db92
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-08-06 23:30:24 +0200

    regles de validation email

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,32 @@
+
+
+// On récupère les deux champs et on affiche leur valeur
+
+let form = document.querySelector("form")
+console.log(form)
+let baliseEmail = document.querySelector("email")
+console.log(email);
+let balisePassword = document.querySelector("password")
+console.log(password);
+
+
+
+//Définition des régles de validation
+
+function verifierChamp(balise){
+    if (balise.value === ""){
+        balise.classList.add("error")
+    } else {
+        balise.classList.remove("error")
+    }
+}
+
+
+function verifierEmail(balise) {
+    let emailRegExp = new RagExp("[a-z0-9._-]+[a-z0-9._-]+\\.[a-z0-9._-]")
+    if(emailRegExp.test(balise.value)) {
+        balise.classList.remove("error")
+    } else {
+        balise.classList.add("error")
+    }
+}
