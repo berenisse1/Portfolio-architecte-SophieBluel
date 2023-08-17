d225f12c2b3033f772a8bb69b3cd137d7ca2ac5a
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-08-17 21:49:38 +0200

    modal update: gestion fenêtre + affichage gallerie

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,5 +1,6 @@
 let urlWorks = `http://localhost:5678/api/works`; // on recupere l'api work
 let section = document.querySelector('.gallery');// on va chercher la class gallery pour aficher les travaux
+let modalGallery = document.querySelector('.modal-gallery') 
 
 
 
@@ -15,9 +16,12 @@ const apiCall = async() => {
             newFigure = document.createElement('figure');
             newFigure.id = element.id;
 
+           
             newImg = document.createElement('img');
             newImg.src = element.imageUrl;
             newImg.alt = element.title;
+            newImg.crossOrigin = "anonymous";
+          
 
             newFigcaption = document.createElement ('figcaption');
             newFigcaption.innerText = element.title;
@@ -30,6 +34,38 @@ const apiCall = async() => {
             
     }
     showWork(apiData);
+
+    function editWork(apiData){
+
+        apiData.forEach(element => { //boucle crée 1 par élement
+
+            newFigure = document.createElement('figure');
+            newFigure.id = element.id;
+
+           
+            newImg = document.createElement('img');
+            newImg.src = element.imageUrl;
+            newImg.alt = element.title;
+            newImg.crossOrigin = "anonymous";
+
+
+            newFigcaption = document.createElement ('figcaption');
+            newFigcaption.innerText = element.title;
+
+            modalGallery.appendChild(newFigure);
+            newFigure.appendChild(newImg);
+            newFigure.appendChild(newFigcaption);
+
+            btnDelete = document.createElement('button');
+            newImg.appendChild(btnDelete);
+            btnDelete.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+            console.log(btnDelete)
+            
+
+        });
+            
+    }
+    editWork(apiData);
 
 
     //Traitement des filtres
@@ -60,11 +96,43 @@ const apiCall = async() => {
         return showWork(apiData);
     });
 
-     // Affichage de la galerie dans la modal
-     document.querySelector(".modal-gallery").innerHTML = showWork(apiData); 
 
 }
 apiCall();
+
+
+// élements de connexion
+let token = localStorage.getItem("token");
+let editModal = document.querySelector(".modal-edit");
+const btnModal = document.querySelectorAll(".btn-modal");
+ 
+if (token) {
+    editModal.style.display = "flex";
+    btnModal.forEach((button) => {
+        button.style.display = "flex";
+    });
+}else{
+    editModal.style.display = "none";
+    btnModal.forEach((button) => {
+        button.style.display = "none";
+    });
+
+}
+console.log(token);
+console.log(editModal);
+console.log(btnModal);
+//elements de déconnexion
+ 
+ 
+if (token) {
+    let log = document.querySelector(".log")
+    log.innerHTML = "logout"
+    log.addEventListener("click", () => {
+        localStorage.removeItem("token")
+        window.location.href = "login.html"
+    });
+}
+
 
 
 // Fonction d'ouverture de la fenetre modal
@@ -73,22 +141,32 @@ let modal = document.querySelector('.modal')
 
 const openModal = function (event){
     event.preventDefault() // empêche rechargement de la page au clic pour rediriger vers url
-    modal.style.display = "flex"; 
-    modal.removeAttribute("aria-hidden") // retire attribut pour garantir accesibilité de la fenêtre modal aux technologie d'assistance
-    modal.setAttribute("aria-modal", "true") // ajoute attribut pour que les technologies d'asistance puisse identifier le type de contenue
+    modal.style.display = 'flex'; 
+    modal.removeAttribute('aria-hidden') // retire attribut pour garantir accesibilité de la fenêtre modal aux technologie d'assistance
+    modal.setAttribute('aria-modal', 'true') // ajoute attribut pour que les technologies d'asistance puisse identifier le type de contenue
     modal.addEventListener("click", closeModal) // apl fonction de fermeture de la modal au clic
+    modal.document.querySelector('.js-modal-close').addEventListener('click',closeModal) 
+    modal.document.querySelector('.js-modal-stop').addEventListener('click',stopPropagation) 
 }
 
 // Fonction de fermeture de la fenêtre modal 
 
 const closeModal = function (event){
     event.preventDefault() 
-    modal.style.display = "none"
-    modal.setAttribute("aria-hiden", "true")
+    modal.style.display = 'none'
+    modal.setAttribute('aria-hiden', 'true')
     modal.removeAttribute("aria-modal")
-    modal.removeEventListener("clisck", closeModal)  
+    modal.removeEventListener('clisck', closeModal)
+    modal.document.querySelector('.js-modal-close').addEventListener('click', closeModal) 
+    modal.document.querySelector('.js-modal-close').removeEventListener('click', closeModal) 
+    modal.document.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation) 
+
+ 
 }
 
+const stopPropagation = function(event){
+    event.stopPropagation()
+}
 
 // Traitement de la fenêtre modale
 
@@ -97,4 +175,6 @@ document.querySelectorAll('.js-modal').forEach(a => { // ajout EventListener sur
     
 })
 
-   
+
+
+
