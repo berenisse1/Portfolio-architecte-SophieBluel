86ad2f33c9e6fb212a2ba01893338cf68bbb2ae2
Berenisse Tabor <berenissetabor@yahoo.fr>
2023-09-05 11:54:48 +0200

    generate category option in modal addWork

--- FrontEnd/assets/category.js
+++ FrontEnd/assets/category.js
@@ -1,7 +1,9 @@
 // Afficher les boutons filtres
+//baliseModalAddWork
 
 let urlCategories = `http://localhost:5678/api/categories`; // on recupere l'api work
 let filters = document.querySelector('.filters');
+const selectCategory = document.querySelector('.selectCategory');
 
 let apiCallCategories = async () => {
 
@@ -36,9 +38,48 @@ let apiCallCategories = async () => {
             
         });
 
-
     }
     filterWork(apiData);
+
+
+    newLabel = document.createElement('label'); 
+    newLabel.innerText = 'Catégorie';
+    newSelect = document.createElement('select');
+
+    newLabel.setAttribute('for', "category");
+    newSelect.setAttribute('name', "category");
+    newSelect.setAttribute('id', "category");
+
+    option = document.createElement ('option');
+    option.setAttribute('value', 0);
+    option.innerText = '';
+    newSelect.appendChild(option);
+
+
+    function CategoryAddWork(apiData){
+
+        
+        apiData.forEach(category => {
+          
+            newOption = document.createElement ('option');
+            newOption.setAttribute('value', category.id);
+            newOption.innerText = category.name;
+
+            selectCategory.appendChild(newLabel);
+            selectCategory.appendChild(newSelect);
+            newSelect.appendChild(newOption);
+            //newSelect.appendChild(newOption);
+        
+            console.log(newLabel);
+            console.log(newSelect);
+            console.log(newOption);
+          
+        });  
+
+       
+       
+    }
+    CategoryAddWork(apiData);
  
 }
 apiCallCategories();
